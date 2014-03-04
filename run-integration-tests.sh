39b26d5858943f91dbc131c5673f28ddc3823be5
John Trimble <trimblej@gmail.com>
2014-03-03 23:49:54 -0700

    If at first you don't succeed...

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -3,7 +3,7 @@ set -e
 # build an example project
 TMP=`mktemp -d /tmp/fullstack.XXXXX`
 cd "$TMP"
-yo cljs-fullstack example
+echo "n" | yo cljs-fullstack example
 # test test
 grunt test
 # test dist
